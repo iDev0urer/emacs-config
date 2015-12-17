3235b22574061ad90f536c567a74bbfc577e7f4b
Chris Watson <cawatson1993@gmail.com>
2015-12-17 13:28:26 -0700

    reinstall on work pc

--- custom.el
+++ custom.el
@@ -3,10 +3,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-margin 50))
+ '(minimap-always-recenter t)
+ '(minimap-dedicated-window t)
+ '(minimap-display-semantic-overlays t)
+ '(minimap-hide-fringes t)
+ '(minimap-hide-scroll-bar t)
+ '(minimap-window-location (quote right))
+ '(scroll-margin 50)
+ '(sublimity-map-criteria
+   (quote
+    ((not
+      (window-minibuffer-p))
+     (or
+      (derived-mode-p
+       (quote prog-mode))
+      (derived-mode-p
+       (quote text-mode)))
+     (<=
+      (/ sublimity-map-size
+         (window-total-width)
+         1.0)
+      sublimity-map-max-fraction))))
+ '(sublimity-map-current-line (quote Courier))
+ '(sublimity-map-text-scale -15))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(minimap-font-face ((t (:height 1.0 :family "Bitstream Charter")))))
---
+++ preload/functions.el
@@ -0,0 +1,11 @@
+;;; package --- Summary;
+;;; Commentary:
+
+;;; Code:
+
+;;; Set theme
+;;; (setq prelude-theme 'twilight)
+(setq prelude-theme 'solarized)
+
+(provide 'functions)
+;;; functions.el ends here
